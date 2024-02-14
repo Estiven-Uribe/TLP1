8d1246434df1b495064904a45d812d6d08088ad6
Estiven-Uribe <114638231+Estiven-Uribe@users.noreply.github.com>
2024-02-13 22:04:20 -0500

    basic code powershell

---
+++ SecondCode.java
@@ -0,0 +1,11 @@
+public class SecondCode {
+
+	public static void main (String[] args) 
+	{
+		int dato = 0;
+		System.out.println("Valor inicial" + dato);
+		dato = 15000;
+		System.out.println("Valor almacenado" + dato);
+		
+	}
+}
